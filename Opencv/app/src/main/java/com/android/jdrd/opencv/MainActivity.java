dd841af34334e14b4fc3972bc3f14f6c347b4195
xuzemin <xuzemin@live.cn>
2016-09-09 18:36:44 +0800

    add for windows opencvdemo

---
+++ Opencv/app/src/main/java/com/android/jdrd/opencv/MainActivity.java
@@ -0,0 +1,27 @@
+package com.android.jdrd.opencv;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Bitmap bitmap = ((BitmapDrawable) getResources().getDrawable(
+                R.mipmap.picture)).getBitmap();
+        int w = bitmap.getWidth(), h = bitmap.getHeight();
+        int[] pix = new int[w * h];
+        bitmap.getPixels(pix, 0, w, 0, 0, w, h);
+        int [] resultPixes=OpenCVHelper.gray(pix,w,h);
+        Bitmap result = Bitmap.createBitmap(w,h, Bitmap.Config.RGB_565);
+        result.setPixels(resultPixes, 0, w, 0, 0,w, h);
+        img = (ImageView)findViewById(R.id.img);
+        img.setImageBitmap(result);
+
+    }
+}
---
+++ Opencv/app/src/main/obj/local/armeabi/objs/OpenCV/com_android_jdrd_opencv_OpenCVHelper.o.d
@@ -0,0 +1,194 @@
+C:/Users/Administrator/Desktop/Opencv/app/src/main/obj/local/armeabi/objs/OpenCV/com_android_jdrd_opencv_OpenCVHelper.o: \
+ C:/Users/Administrator/Desktop/Opencv/app/src/main/jni/com_android_jdrd_opencv_OpenCVHelper.cpp \
+ C:/Users/Administrator/Desktop/Opencv/app/src/main/jni/com_android_jdrd_opencv_OpenCVHelper.h \
+ ../../../../native/jni/include/opencv2/opencv.hpp \
+ ../../../../native/jni/include/opencv2/core.hpp \
+ ../../../../native/jni/include/opencv2/core/cvdef.h \
+ ../../../../native/jni/include/opencv2/hal/defs.h \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ ../../../../native/jni/include/opencv2/core/version.hpp \
+ ../../../../native/jni/include/opencv2/core/base.hpp \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+ ../../../../native/jni/include/opencv2/core/cvstd.hpp \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ ../../../../native/jni/include/opencv2/core/ptr.inl.hpp \
+ ../../../../native/jni/include/opencv2/hal.hpp \
+ ../../../../native/jni/include/opencv2/core/sse_utils.hpp \
+ ../../../../native/jni/include/opencv2/core/traits.hpp \
+ ../../../../native/jni/include/opencv2/core/matx.hpp \
+ ../../../../native/jni/include/opencv2/core/types.hpp \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ ../../../../native/jni/include/opencv2/core/mat.hpp \
+ ../../../../native/jni/include/opencv2/core/bufferpool.hpp \
+ ../../../../native/jni/include/opencv2/core/mat.inl.hpp \
+ ../../../../native/jni/include/opencv2/core/persistence.hpp \
+ ../../../../native/jni/include/opencv2/core/operations.hpp \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+ ../../../../native/jni/include/opencv2/core/cvstd.inl.hpp \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ ../../../../native/jni/include/opencv2/core/utility.hpp \
+ ../../../../native/jni/include/opencv2/core/core_c.h \
+ ../../../../native/jni/include/opencv2/core/types_c.h \
+ ../../../../native/jni/include/opencv2/core/optim.hpp \
+ ../../../../native/jni/include/opencv2/imgproc.hpp \
+ ../../../../native/jni/include/opencv2/imgproc/imgproc_c.h \
+ ../../../../native/jni/include/opencv2/imgproc/types_c.h \
+ ../../../../native/jni/include/opencv2/photo.hpp \
+ ../../../../native/jni/include/opencv2/photo/photo_c.h \
+ ../../../../native/jni/include/opencv2/video.hpp \
+ ../../../../native/jni/include/opencv2/video/tracking.hpp \
+ ../../../../native/jni/include/opencv2/video/background_segm.hpp \
+ ../../../../native/jni/include/opencv2/video/tracking_c.h \
+ ../../../../native/jni/include/opencv2/features2d.hpp \
+ ../../../../native/jni/include/opencv2/flann/miniflann.hpp \
+ ../../../../native/jni/include/opencv2/flann/defines.h \
+ ../../../../native/jni/include/opencv2/flann/config.h \
+ ../../../../native/jni/include/opencv2/objdetect.hpp \
+ ../../../../native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ ../../../../native/jni/include/opencv2/objdetect/objdetect_c.h \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/deque \
+ ../../../../native/jni/include/opencv2/calib3d.hpp \
+ ../../../../native/jni/include/opencv2/core/affine.hpp \
+ ../../../../native/jni/include/opencv2/calib3d/calib3d_c.h \
+ ../../../../native/jni/include/opencv2/imgcodecs.hpp \
+ ../../../../native/jni/include/opencv2/videoio.hpp \
+ ../../../../native/jni/include/opencv2/highgui.hpp \
+ ../../../../native/jni/include/opencv2/highgui/highgui_c.h \
+ ../../../../native/jni/include/opencv2/imgcodecs/imgcodecs_c.h \
+ ../../../../native/jni/include/opencv2/videoio/videoio_c.h \
+ ../../../../native/jni/include/opencv2/ml.hpp \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+ C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/iostream
+
+C:/Users/Administrator/Desktop/Opencv/app/src/main/jni/com_android_jdrd_opencv_OpenCVHelper.h:
+
+../../../../native/jni/include/opencv2/opencv.hpp:
+
+../../../../native/jni/include/opencv2/core.hpp:
+
+../../../../native/jni/include/opencv2/core/cvdef.h:
+
+../../../../native/jni/include/opencv2/hal/defs.h:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+../../../../native/jni/include/opencv2/core/version.hpp:
+
+../../../../native/jni/include/opencv2/core/base.hpp:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+../../../../native/jni/include/opencv2/core/cvstd.hpp:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+../../../../native/jni/include/opencv2/core/ptr.inl.hpp:
+
+../../../../native/jni/include/opencv2/hal.hpp:
+
+../../../../native/jni/include/opencv2/core/sse_utils.hpp:
+
+../../../../native/jni/include/opencv2/core/traits.hpp:
+
+../../../../native/jni/include/opencv2/core/matx.hpp:
+
+../../../../native/jni/include/opencv2/core/types.hpp:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+../../../../native/jni/include/opencv2/core/mat.hpp:
+
+../../../../native/jni/include/opencv2/core/bufferpool.hpp:
+
+../../../../native/jni/include/opencv2/core/mat.inl.hpp:
+
+../../../../native/jni/include/opencv2/core/persistence.hpp:
+
+../../../../native/jni/include/opencv2/core/operations.hpp:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+../../../../native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+../../../../native/jni/include/opencv2/core/utility.hpp:
+
+../../../../native/jni/include/opencv2/core/core_c.h:
+
+../../../../native/jni/include/opencv2/core/types_c.h:
+
+../../../../native/jni/include/opencv2/core/optim.hpp:
+
+../../../../native/jni/include/opencv2/imgproc.hpp:
+
+../../../../native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+../../../../native/jni/include/opencv2/imgproc/types_c.h:
+
+../../../../native/jni/include/opencv2/photo.hpp:
+
+../../../../native/jni/include/opencv2/photo/photo_c.h:
+
+../../../../native/jni/include/opencv2/video.hpp:
+
+../../../../native/jni/include/opencv2/video/tracking.hpp:
+
+../../../../native/jni/include/opencv2/video/background_segm.hpp:
+
+../../../../native/jni/include/opencv2/video/tracking_c.h:
+
+../../../../native/jni/include/opencv2/features2d.hpp:
+
+../../../../native/jni/include/opencv2/flann/miniflann.hpp:
+
+../../../../native/jni/include/opencv2/flann/defines.h:
+
+../../../../native/jni/include/opencv2/flann/config.h:
+
+../../../../native/jni/include/opencv2/objdetect.hpp:
+
+../../../../native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+../../../../native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
+
+../../../../native/jni/include/opencv2/calib3d.hpp:
+
+../../../../native/jni/include/opencv2/core/affine.hpp:
+
+../../../../native/jni/include/opencv2/calib3d/calib3d_c.h:
+
+../../../../native/jni/include/opencv2/imgcodecs.hpp:
+
+../../../../native/jni/include/opencv2/videoio.hpp:
+
+../../../../native/jni/include/opencv2/highgui.hpp:
+
+../../../../native/jni/include/opencv2/highgui/highgui_c.h:
+
+../../../../native/jni/include/opencv2/imgcodecs/imgcodecs_c.h:
+
+../../../../native/jni/include/opencv2/videoio/videoio_c.h:
+
+../../../../native/jni/include/opencv2/ml.hpp:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+C:/Users/Administrator/AppData/Local/Android/sdk/ndk-bundle/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
