071596da22ddbdfb70aff696eadf3be5f39e6f71
xuzemin <xuzemin@live.cn>
2017-03-30 16:06:57 +0800

    update for BodyControl to shutdown

--- BodyControl/src/com/jdrd/bodyControl/main/BodyControl.java
+++ BodyControl/src/com/jdrd/bodyControl/main/BodyControl.java
@@ -8,26 +8,32 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class BodyControl {
-	private static String IP_ADDR = "192.168.1.106";
+	private static String IP_ADDR = "192.168.20.13";
 	private static int PORT = 12345;
+	private static Process ARproc = null;
+	private static Process ADproc = null;
 	public static void main(String[] args) {
 		while(true){
-			 Socket socket = null;  
+			Socket socket = null;  
+			InputStream inputStream = null;
+			PrintWriter out = null;
 			try {
 				socket = new Socket(IP_ADDR,PORT);
-				PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "GBK")), true);
-//				socket.setKeepAlive(true);
-				InputStream inputStream = socket.getInputStream();
+				out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream(), "GBK")), true);
+				inputStream = socket.getInputStream();
 				byte buffer[] = new byte[1024 * 4];
 				int temp = 0;
+				out.println("connect");
 				while ((temp = inputStream.read(buffer)) != -1) {
 					String msg = new String(buffer, 0, temp, "GBK");
 					System.out.println(msg);
-					out.println("i recieved");
-					if(msg.equals("AR")){
+					out.println("success");
+					if(msg.equals("openAR")){
 						openAR();
-					}else{
-						openNotepad();
+					}else if(msg.equals("openAD")){
+						openAD();
+					}else if(msg.equals("close")){
+						close();
 					}
 				}
 				out.close(); 
@@ -36,26 +42,40 @@ public class BodyControl {
 			}catch(IOException e) { 
 				e.printStackTrace();
 			}
+		
 		}
 	}
-	private static void openNotepad(){
+	private static void openAD(){
 		Runtime rt = Runtime.getRuntime();
-		Process p = null;
 		String fileLac = "";
 		try {
-			fileLac = "E:\\Program Files (x86)\\Tencent\\WeChat\\WeChat.exe";
-			p = rt.exec(fileLac);
+			if(ARproc !=null){
+				ARproc.destroy();
+			}
+			fileLac = "E:\\Program Files (x86)\\Notepad++\\notepad++.exe";
+			ADproc = rt.exec(fileLac);
+			Thread.sleep(1000);
 		} catch (Exception e) {
 			System.out.println("open failure");
 		}
 	}
 	private static void openAR(){
 		Runtime rt = Runtime.getRuntime();
-		Process p = null;
 		String fileLac = "";
 		try {
-			fileLac = "E:\\Program Files (x86)\\Tencent\\QQ\\Bin\\QQScLauncher.exe";
-			p = rt.exec(fileLac);
+			if(ADproc != null){
+				ADproc.destroy();
+			}
+			fileLac = "E:\\Program Files (x86)\\Navicat for MySQL\\navicat.exe";
+			ARproc = rt.exec(fileLac);
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			System.out.println("open failure");
+		}
+	}
+	private static void close(){
+		try {
+			Runtime.getRuntime().exec("shutdown -s -t 1"); 
 		} catch (Exception e) {
 			System.out.println("open failure");
 		}
