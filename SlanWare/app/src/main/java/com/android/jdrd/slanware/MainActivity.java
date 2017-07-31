6b62993c3afa38423c139112f6d037e959212c11
xuzemin <xuzemin@live.cn>
2017-07-31 17:11:28 +0800

    add for SlanWare

---
+++ SlanWare/app/src/main/java/com/android/jdrd/slanware/MainActivity.java
@@ -0,0 +1,35 @@
+package com.android.jdrd.slanware;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.slamtec.slamware.AbstractSlamwarePlatform;
+import com.slamtec.slamware.discovery.DeviceManager;
+import com.slamtec.slamware.robot.Pose;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+//    static {
+//        System.loadLibrary("native-lib");
+//    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+//        TextView tv = (TextView) findViewById(R.id.sample_text);
+//        tv.setText(stringFromJNI());
+        AbstractSlamwarePlatform platform = DeviceManager.connect("192.168.11.1",1445);
+        Pose pose = platform.getPose();
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}
