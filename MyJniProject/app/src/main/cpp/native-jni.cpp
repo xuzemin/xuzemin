128d616382d18bc625c7fd566e672240e4113e6b
xuzemin <xuzemin@live.cn>
2018-11-07 20:58:43 +0800

    add for JNi Project

--- MyJniProject/app/src/main/cpp/native-jni.cpp
+++ MyJniProject/app/src/main/cpp/native-jni.cpp
@@ -2,9 +2,9 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_jni_android_myjniproject_MainActivity_stringFromJNI(
-        JNIEnv* env,
+Java_com_jni_android_myjniproject_JniUtils_stringFromJNI(
+        JNIEnv *env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "深圳市智宝机器人科技有限公司";
     return env->NewStringUTF(hello.c_str());
 }
