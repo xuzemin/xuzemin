a9bec221e842a448346a03b59e39d07ffd4b0535
xuzemin <xuzemin@live.cn>
2019-01-01 23:09:16 +0800

    更新服务器数据库

--- Server/database/createsql.sql
+++ Server/database/createsql.sql
@@ -1,9 +1,17 @@
+drop DATABASE base;
 CREATE DATABASE Base;
-use  Base;
-create  TABLE  userInfo (userId int NOT NULL auto_increment primary key AUTO_INCREMENT,userName VARCHAR(20) NOT NULL unique
+USE Base;
+create  TABLE  userInfo (userId int NOT NULL auto_increment primary key,userName VARCHAR(20) NOT NULL unique
 					,userPassword VARCHAR(20) NOT NULL,Sex int,userType int Not NULL,lastLogintime timestamp
-                    ,macAddress VARCHAR(20),userUsename VARCHAR(20),personNumber varchar(20),personName VARCHAR(20));
-create  TABLE  userType (typeId int NOT NULL auto_increment primary key,typeName VARCHAR(20));
-INSERT  INTO  userInfo (userName,userPassword,userType) VALUES ('huzhiheng','123456',1); 
+                    ,macAddress VARCHAR(20),userUsename VARCHAR(20) unique,personNumber varchar(20));
+create  TABLE  userType (typeId int NOT NULL auto_increment primary key,typeName VARCHAR(20) unique);
+create  TABLE  apkversion (Id int NOT NULL auto_increment primary key,versionName VARCHAR(20),
+versionCode int(10),pathurl VARCHAR(20) ,package VARCHAR(20),packageName VARCHAR(20),lastLogintime timestamp unique);
+
+create  TABLE  uploadRecord (Id int NOT NULL auto_increment primary key, userId int ,userFor int,uploadTime timestamp);
+create  TABLE  filePath (Id int NOT NULL auto_increment primary key,uploadid int ,userid int,filePath VARCHAR(20),filename varchar(20));
+
+INSERT  INTO  userInfo (userName,userPassword,userType) VALUES ('xzm','123',1); 
 INSERT  INTO  userType (typeName) VALUES ('personnal'); 
 INSERT  INTO  userType (typeName) VALUES ('company'); 
+INSERT  INTO  apkversion (versionName,versionCode,pathurl,package,packageName) VALUES ('1.0.0',1,"com.android.example","/webapp/","Lottery"); 
--- Server/database/testsql.sql
+++ Server/database/testsql.sql
@@ -1,9 +1,15 @@
-use base
-select * from userInfo
-select * from usertype
+use base;
+select * from userInfo;
+select * from apkversion;
+select * from filePath;
+select * from uploadRecord;
+select * from userInfo where userName = 'xzm' and userPassword = '123' and userId = '1';
 
-drop database base
 
-INSERT  INTO  user (userName,userPassword,userType) VALUES ('xzm','123'，0); 
+select * from uploadRecord where userFor = '0'  and uploadTime = '2018-12-21 01:02:31.074'  and userId = '1' 
+select * from uploadRecord where userFor = '0'  and uploadTime = '2018-12-21 01:08:11.321'  and userId = '1' 
+drop database base;
+
 INSERT  INTO  userInfo (userName,userPassword,userType) VALUES ('xzm','123',1); 
-update userinfo set userUsename = '徐泽民' where userId = 1
+
+update userinfo set userUsename = '徐泽民' where userId = 1;
